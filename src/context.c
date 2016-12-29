0878f101e3a18001d7b0dcc77c89aa9b3f6575ff
Tobias Frost <tobi@coldtobi.de>
2016-12-29 16:41:35 +0100

    First version of the CCT color loop -- completly untested, it barely compiles!

--- src/context.c
+++ src/context.c
@@ -340,6 +340,52 @@ enum msg_0xd8_answer {
 	ANSWER_0xD8_SIZE
 };
 
+enum msg_0xd9_query {
+	QUERY_0xD9_NODEADR64_B0 = HEADER_PAYLOAD_START,
+	QUERY_0xD9_NODEADR64_B1,
+	QUERY_0xD9_NODEADR64_B2,
+	QUERY_0xD9_NODEADR64_B3,
+	QUERY_0xD9_NODEADR64_B4,
+	QUERY_0xD9_NODEADR64_B5,
+	QUERY_0xD9_NODEADR64_B6,
+	QUERY_0xD9_NODEADR64_B7,
+	QUERY_0xD9_UNKNOWN_1,
+	QUERY_0xD9_UNKNOWN_2,
+	QUERY_0xD9_UNKNOWN_3,
+	QUERY_0xD9_UNKNOWN_4,
+	QUERY_0xD9_UNKNOWN_5,
+	QUERY_0xD9_UNKNOWN_6,
+	QUERY_0xD9_UNKNOWN_7,
+	QUERY_0xD9_UNKNOWN_8,
+	QUERY_0xD9_START_OF_PROGRAMM,
+	/* now, the program starts. */
+};
+
+enum msg_0xd9_step {
+	STEP_0xD9_SPEED,
+	STEP_0xD9_HUE,
+	STEP_0xD9_SATURATION,
+	STEP_0xD9_BRIGHTNESS,
+	STEP_0xD9_SIZE
+};
+
+enum msg_0xd9_answer {
+	ANSWER_0xD9_STATE = HEADER_PAYLOAD_START,
+	ANSWER_0xD9_RESERVED_LSB,
+	ANSWER_0xD9_RESERVED_MSB,
+	ANSWER_0xD9_NODEADR64_B0,
+	ANSWER_0xD9_NODEADR64_B1,
+	ANSWER_0xD9_NODEADR64_B2,
+	ANSWER_0xD9_NODEADR64_B3,
+	ANSWER_0xD9_NODEADR64_B4,
+	ANSWER_0xD9_NODEADR64_B5,
+	ANSWER_0xD9_NODEADR64_B6,
+	ANSWER_0xD9_NODEADR64_B7,
+	ANSWER_0xD9_UNKNOWN1,
+	ANSWER_0xD9_SIZE
+};
+
+
 // 0 seems success, non-zero error.
 static int decode_status(unsigned char code) {
 	switch (code) {
@@ -1371,5 +1417,100 @@ LIGHTIFY_EXPORT int lightify_node_request_color_loop(struct lightify_ctx *ctx,
 
 	n = -decode_status(msg[ANSWER_0xD8_STATE]);
 	dbg(ctx, "unknown-byte: %x\n", msg[ANSWER_0xD8_UNKNOWN1]);
+	return n;
+}
+
+
+// note: experimental API -- do not use yet.
+// WARNING: INSTABLE API.
+LIGHTIFY_EXPORT int lightify_node_request_cct_loop(struct lightify_ctx *ctx,
+		struct lightify_node *node, const struct lightify_cct_loop_spec* cctspec,
+		unsigned int number_of_specs, const uint8_t static_bytes[8])
+{
+	if (!ctx || !node || !cctspec || 15 != number_of_specs) return -EINVAL;
+
+	// not finsihed API -- completly untested!
+	return -EIO;
+
+	/* cctspec[0].delay must be 0x3C */
+	if (cctspec[0].delay != 0x3C) return -EINVAL;
+
+	unsigned int telegram_size = QUERY_0xD9_START_OF_PROGRAMM + 1 + number_of_specs*STEP_0xD9_SIZE;
+	unsigned char msg[telegram_size +1];
+
+	uint32_t token = ++ctx->cnt;
+	fill_telegram_header(msg, telegram_size, token, 0, 0xd8);
+
+	uint64_t adr = lightify_node_get_nodeadr(node);
+	msg_from_uint64(&msg[QUERY_0xD9_NODEADR64_B0], adr);
+
+	/* static 8 bytes: 01 ff 00 ff 00 3c 00 00
+	 * unclear meaning -- some bytes seems to change behaviour,
+	 * need to fiddle with it -- this is why this api is not stable yet.*/
+	if (!static_bytes) {
+		const unsigned char statics[8] = {0x01, 0xff, 0x00, 0xff, 0x00, 0x3c, 0x00, 0x00};
+		memcpy(&msg[QUERY_0xD9_UNKNOWN_1],statics,sizeof(statics));
+	} else {
+		memcpy(&msg[QUERY_0xD9_UNKNOWN_1],static_bytes,8);
+	}
+
+	// Fill colorspecs
+	uint8_t *ptr = &msg[QUERY_0xD8_START_OF_PROGRAMM];
+	unsigned int i=0;
+	do {
+		*ptr++ = cctspec[i].delay;
+		*ptr++ = (cctspec[i].cct / 10) >> 8 ;
+		*ptr++ = (cctspec[i].cct / 10) & 0xFF;
+		*ptr++ = cctspec[i].brightness;
+		i++;
+	} while (i < number_of_specs);
+
+	/* Calculate the checksum:
+	 * Checksum is: start with 0xff and substract every byte over all color-specs,
+	 * except the first byte (which is static 0x3c) */
+	unsigned int checksum = 0xff;
+	uint8_t *ptr2 = (uint8_t*) cctspec + 1 ;
+	size_t size = number_of_specs * STEP_0xD9_SIZE;
+	for (i=1; i < size; i++) {
+		checksum -= *ptr2++;
+	}
+	*ptr++ = checksum & 0xff;
+
+	int n = ctx->socket_write_fn(ctx,msg, telegram_size);
+	if ( n < 0 ) {
+		info(ctx,"socket_write_fn error %d\n", n);
+		return n;
+	}
+	if ( n != (int) telegram_size) {
+		info(ctx,"short write %d!=%d\n", telegram_size, n);
+		return -EIO;
+	}
+
+	/* read the header */
+	n = ctx->socket_read_fn(ctx, msg, ANSWER_0xD9_SIZE);
+	if (n < 0) {
+		info(ctx,"socket_read_fn error %d\n", n);
+		return n;
+	}
+	if (n != ANSWER_0xD8_SIZE) {
+		info(ctx,"short read %d!=%d\n", ANSWER_0xD9_SIZE, n);
+		return -EIO;
+	}
+
+	n = check_header_response(msg, token, 0xD9);
+	if ( n < 0 ) {
+		info(ctx,"Invalid response (header)\n");
+		return n;
+	}
+
+	/* check if the node address was echoed properly */
+	uint64_t adr2 = uint64_from_msg(&msg[ANSWER_0xD9_NODEADR64_B0]);
+	if (adr != adr2) {
+		info(ctx, "unexpected node mac / group adr %llx!=%llx", adr, adr2 );
+		return -EPROTO;
+	}
+
+	n = -decode_status(msg[ANSWER_0xD9_STATE]);
+	dbg(ctx, "unknown-byte: %x\n", msg[ANSWER_0xD9_UNKNOWN1]);
 	return n;
 }
